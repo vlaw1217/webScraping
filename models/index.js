38975fede110378bea85ed25ff8ad8707bd7a269
Vicky Law <vicky.law1217@gmail.com>
2019-10-26 12:14:38 -0400

    rename file for case sensitivity

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //Exporting an object containing all of our models
 
 module.exports = {
-  Stories: require("./Stories"),
-  UserComment: require("./UserComment")
+  Stories: require("./stories"),
+  UserComment: require("./user-comment")
 };
