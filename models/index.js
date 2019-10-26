745eb0e447cc782be3809f73aa8b422319d1e205
Vicky Law <vicky.law1217@gmail.com>
2019-10-26 12:47:07 -0400

    name change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //Exporting an object containing all of our models
 
 module.exports = {
-  Stories: require("./stories"),
+  Stories: require("./ssstories"),
   UserComment: require("./user-comment")
 };
