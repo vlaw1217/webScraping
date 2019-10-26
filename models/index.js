ec7389c37f45adc7f9593209e2517bf19b6ca160
Vicky Law <vicky.law1217@gmail.com>
2019-10-25 21:26:51 -0400

    testing1

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //Exporting an object containing all of our models
 
 module.exports = {
-  Stories: require("./Stories"),
+  Stories: require("./stories"),
   UserComment: require("./UserComment")
 };
